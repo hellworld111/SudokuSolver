bfdc32029ff46a2de903825841a1ee851e1b3801
Yugo Igarashi <yugoigarashi@Yugos-MacBook-Air.local>
2025-03-02 09:50:39 +0900

    first commit

---
+++ sudoku.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+
+//first validate each number, and return false if not
+//if invalid, remove from, sudoku board
+//fil numbers 1-9, check if valid every time
+//If invalid, backtrack to the last number 
