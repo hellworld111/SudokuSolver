00593d8ad4415356e2b2f9a2e63a781dd5f7ba70
Yugo Igarashi <yugoigarashi@Yugos-MacBook-Air.local>
2025-03-02 10:00:43 +0900

    changed file status

--- sudoku.c
+++ sudoku.c
@@ -6,5 +6,5 @@
 
 //first validate each number, and return false if not
 //if invalid, remove from, sudoku board
-//fil numbers 1-9, check if valid every time
+//fil numbers 1-9, check if valid every timesos
 //If invalid, backtrack to the last number 
